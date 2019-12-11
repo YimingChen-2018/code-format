5a2c4a280149bf022921f11f0f0e8f3288a2cd8c
AngelaChen_1992 <yimingchen_2018@163.com>
2019-12-11 21:20:16 +0800

    测试代码规范

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/recommended", "@vue/prettier"],
+  rules: {},
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+}
